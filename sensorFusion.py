7dd645593f3c40b07cc8fde32c2976a78a292e51
smahajan07 <mahajan.sarthak28@gmail.com>
2018-11-08 23:40:52 -0500

    calling all methods from main

--- sensorFusion.py
+++ sensorFusion.py
@@ -8,41 +8,75 @@ from helperMethods import helperMethods
 ACTUAL_GRAVITY = 9.80665
 
 # open json file with collected data
-file_name = 'test.json'
+file_name = 'pos_final.json'
 with open(file_name) as data_file:
   data = json.load(data_file)
-# read initial data
-initialData = data[0]
+  # read initial data
+  initialData = data[0]
 
-# set standard deviations
-latLonStdDev = 2.0
-altStdDev = 3.518522417151836
-accEastStdDev = ACTUAL_GRAVITY * 0.033436506994600976
-accNorthStdDev = ACTUAL_GRAVITY * 0.05355371135598354
-accUpStdDev = ACTUAL_GRAVITY * 0.2088683796078286
+  # set standard deviations
+  latLonStdDev = 2.0
+  altStdDev = 3.518522417151836
+  accEastStdDev = ACTUAL_GRAVITY * 0.033436506994600976
+  accNorthStdDev = ACTUAL_GRAVITY * 0.05355371135598354
+  accUpStdDev = ACTUAL_GRAVITY * 0.2088683796078286
 
-# create object of helper class
-helperObj = helperMethods()
+  # create object of helper class
+  helperObj = helperMethods()
 
-# create objects of kalman filter
-objEast = kalmanFilter(helperObj.lonToMtrs(initialData["gps_lon"]), \
-                    initialData["vel_east"], latLonStdDev, \
-                    accEastStdDev, initialData["timestamp"])
+  # create objects of kalman filter
+  objEast = kalmanFilter(helperObj.lonToMtrs(initialData["gps_lon"]), \
+                      initialData["vel_east"], latLonStdDev, \
+                      accEastStdDev, initialData["timestamp"])
 
-# for debugging
-# print(objEast.X)
-# print(objEast.Q)
-# print(objEast.R)
-# print(objEast.currStateTime)
+  # for debugging
+  # print(objEast.X)
+  # print(objEast.Q)
+  # print(objEast.R)
+  # print(objEast.currStateTime)
 
-objNorth = kalmanFilter(helperObj.latToMtrs(initialData["gps_lat"]), \
-                    initialData["vel_north"], latLonStdDev, \
-                    accNorthStdDev, initialData["timestamp"])
+  objNorth = kalmanFilter(helperObj.latToMtrs(initialData["gps_lat"]), \
+                      initialData["vel_north"], latLonStdDev, \
+                      accNorthStdDev, initialData["timestamp"])
 
-objUp = kalmanFilter(initialData["gps_alt"], \
-                    initialData["vel_down"] * -1.0, latLonStdDev, \
-                    accUpStdDev, initialData["timestamp"])
+  objUp = kalmanFilter(initialData["gps_alt"], \
+                      initialData["vel_down"] * -1.0, latLonStdDev, \
+                      accUpStdDev, initialData["timestamp"])
 
-# # run loop over new readings
-for i in range(1,len(data)):
-  pass
+  # # run loop over new readings
+  for i in range(1,10): #len(data)
+    currData = data[i]
+
+    objEast.predict(currData["abs_east_acc"] * ACTUAL_GRAVITY, currData["timestamp"])
+    objNorth.predict(currData["abs_north_acc"] * ACTUAL_GRAVITY, currData["timestamp"])
+    objUp.predict(currData["abs_up_acc"] * ACTUAL_GRAVITY, currData["timestamp"])
+
+    if(not currData["gps_lat"]):
+
+      defPosErr = 0
+
+      vEast = currData["vel_east"]
+      longitude = objEast.lonToMtrs(currData["gps_lon"])
+      objEast.update(longitude, vEast, defPosErr, currData["vel_error"])
+
+      vNorth = currData["vel_north"]
+      latitude = objNorth.latToMtrs(currData["gps_lat"])
+      objNorth.update(latitude, vNorth, defPosErr, currData["vel_error"])
+
+      vUp = currData["vel_down"] * -1.0
+      objUp.update(currData["gps_alt"], vUp, currData["altitude_error"], currData["vel_error"])
+
+    predictedLonMtrs = objEast.getPredictedPos()
+    predictedLatMtrs = objNorth.getPredictedPos()
+    predictedAlt = objUp.getPredictedPos()
+
+    predictedLat, predictedLon = helperObj.mtrsToGeopoint(predictedLatMtrs, predictedLonMtrs)
+
+    predictedVE = objEast.getPredictedVel()
+    predictedVN = objNorth.getPredictedVel()
+
+    resultantV = np.sqrt(np.power(predictedVE, 2) + np.power(predictedVN, 2))
+    deltaT = currData["timestamp"] - initialData["timestamp"]
+
+    print("{} seconds in, Lat: {}, Lon: {}, Alt: {}, Vel(mph): {}".format(
+            deltaT, predictedLat, predictedLon, predictedAlt, resultantV))
